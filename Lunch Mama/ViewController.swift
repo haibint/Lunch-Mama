acfed44dffce37623824bff1d4b74300d2674792
Roytangrb <roytang.rb@gmail.com>
2019-03-28 22:58:23 +0800

    Initial Commit

---
+++ Lunch Mama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lunch Mama
+//
+//  Created by 湯銳彬 on 28/3/2019.
+//  Copyright © 2019 binaryyard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
