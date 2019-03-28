b66c372a467822c4d037ed0b8d3172a54639fc1a
Roytangrb <roytang.rb@gmail.com>
2019-03-28 23:54:35 +0800

    initial commit added login page

--- Lunch Mama/ViewController.swift
+++ Lunch Mama/ViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginID: UITextField!
+    @IBOutlet weak var loginPW: UITextField!
+    //resign first responder while submit
+    @IBAction func loginSubmit(_ sender: UIButton) {
+        self.loginID.resignFirstResponder()
+        self.loginPW.resignFirstResponder()
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    //change login role image
+    @IBOutlet weak var currentLoginRoleIcon: UIImageView!
+    @IBAction func loginRoleControl(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: //student
+            currentLoginRoleIcon.image = UIImage(named: "loginstudent")
+            break
+        case 1: //staff
+            currentLoginRoleIcon.image = UIImage(named: "loginteacher")
+            break
+        case 2: //admin
+            currentLoginRoleIcon.image = UIImage(named: "loginadmin")
+            break
+        default:
+            currentLoginRoleIcon.image = UIImage(named: "loginpic")
+            break
+        }
+    }
+    
+    //cancel first responder on backgound touch
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
