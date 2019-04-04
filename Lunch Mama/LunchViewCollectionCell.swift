1e8cba6a3bdb97b3e652d9828d5d9f72163bfefc
haibint <676544964.st@gmail.com>
2019-04-05 01:38:26 +0800

    added UIcollection View for lunch record scene

---
+++ Lunch Mama/LunchViewCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  LunchViewCollectionCellCollectionViewCell.swift
+//  Lunch Mama
+//
+//  Created by 湯海彬 on 5/4/2019.
+//  Copyright © 2019 binaryyard. All rights reserved.
+//
+
+import UIKit
+
+class LunchViewCollectionCellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var my_label: UILabel!
+    
+}
